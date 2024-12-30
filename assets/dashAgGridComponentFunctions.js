85e0746558d89f41a115d5fe8c912ce11a33d8f3
Natalia <nataliatsyporkin@gmail.com>
2024-12-30 03:56:03 +0200

    Update dashAgGridComponentFunctions.js

--- assets/dashAgGridComponentFunctions.js
+++ assets/dashAgGridComponentFunctions.js
@@ -1,26 +1,5 @@
-
-var dagcomponentfuncs = window.dashAgGridComponentFunctions = window.dashAgGridComponentFunctions || {};
-
-dagcomponentfuncs.DCC_GraphClickData = function (props) {
-    const {setData} = props;
-    function setProps() {
-        const graphProps = arguments[0];
-        if (graphProps['clickData']) {
-            setData(graphProps);
-        }
-    }
-    return React.createElement(window.dash_core_components.Graph, {
-        figure: props.value,
-        setProps,
-        style: {height: '100%'},
-        config: {displayModeBar: false},
-    });
-};
-
-
-
 /*
-This modification simplifies the function to display only the graph component
+This modification simplifies the function to display only the graph component*/
 
 var dagcomponentfuncs = window.dashAgGridComponentFunctions = window.dashAgGridComponentFunctions || {};
 
@@ -32,4 +11,3 @@ dagcomponentfuncs.DCC_GraphClickData = function (props) {
     });
 };
 
-*/
